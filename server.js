a79cdf09a153e5ffaee5942c47e35f469ec6a769
Chandan <cs762585@gmail.com>
2026-06-28 17:28:18 +0530

    updated package.json

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running successfully 🚀");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
